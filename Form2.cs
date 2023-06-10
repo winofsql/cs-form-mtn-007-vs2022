bec124a10ebef5ac707df836a28ad0d4b7755cf3
sworc <lightbox@nifty.com>
2023-06-10 19:59:00 +0900

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,29 @@
+﻿namespace cs_form_mtn_007_vs2022
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        // *****************************************
+        // ファンクションキ－
+        // ( フォームの KeyPreview : True )
+        // *****************************************
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                // このダイアログを閉じる
+                this.Close();
+                // このダイアログの終了時のフラグ
+                this.DialogResult = DialogResult.OK;
+                // 参照する為に modifiers を internal に
+                ((Form1)this.Owner).社員コード.Text = "0001";
+            }
+        }
+
+
+    }
+}
